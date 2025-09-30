62e491ac3e7817fb9cc31fe949d3c4206c6ea9b4
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2025-09-30 09:35:28 +0200

    add AbsoluteFilmora image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/organisms/Navbar";
 import {Box} from "@mui/system";
+import Image from "next/image";
+import AbsoluteFilmora from "@/assets/AbsoluteFilmora.png";
 
 export default function Home() {
     return (
@@ -12,7 +14,7 @@ export default function Home() {
                 justifyContent: "center",
                 alignItems: "center"
             }}>
-
+                <Image src={AbsoluteFilmora} alt="Absolute Filmora" />
 
             </Box>
         </>
