c1e2d1278829ce249ecab3c3fbd99f511c7110f6
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2025-11-01 16:07:39 +0100

    change styling

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -2,23 +2,54 @@ import {Box} from "@mui/system";
 import {Typography} from "@mui/material";
 import NavButton from "@/components/atoms/NavButton";
 
+interface NavbarProps {
+    details?: boolean;
+}
 
-const Navbar = () => {
+const Navbar = ({ details = false }: NavbarProps) => {
     return (
-        <Box sx={{
-            backgroundColor: "#595874",
-            height: "100px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <Typography variant="h1">
+        <Box
+            sx={{
+                backgroundColor: "rgba(89, 88, 116, 0.95)",
+                height: "100px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingRight: "100px",
+                paddingLeft: "100px",
+                position: "sticky",
+                top: 0,
+                zIndex: 999,
+                width: "100%",
+                boxShadow: "0 2px 10px rgba(0,0,0,0.2)",
+            }}
+        >
+            <Typography
+                variant="h3"
+                sx={{color: "#fff", fontWeight: "bold", userSelect: "none"}}
+            >
                 ABSOLUTE FILMORA
             </Typography>
 
-            <NavButton path={"/"} name={"Home"}/>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "1rem",
+                }}
+            >
+                {details ? (
+                    <NavButton path="/overview" name="<-" />
+                ) : (
+                    <>
+                        <NavButton path="/" name="Home" />
+                        <NavButton path="/overview" name="Movies" />
+                    </>
+                )}
+            </Box>
         </Box>
-    )
-}
+    );
+};
 
 export default Navbar;
